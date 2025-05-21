cf88a06c259b7678fb3082aaf46310e4f2b2b6ce
merlin1320 <littlenights447@gmail.com>
2025-05-20 22:17:19 -0600

    Added population of information

--- gamescores.sql
+++ gamescores.sql
@@ -11,18 +11,17 @@ DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
 
 
-CREATE TABLE `mysql-chal`.Users (
-	id BIGINT auto_increment NOT NULL,
-	user_type_id BIGINT NOT NULL,
-	username varchar(255) NOT NULL,
-	last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP NOT NULL,
-	PRIMARY KEY (id),
-	CONSTRAINT Users_UNIQUE UNIQUE KEY (username),
-	CONSTRAINT Users_UserTypes_FK FOREIGN KEY (id) REFERENCES `mysql-chal`.UserTypes(id) ON DELETE CASCADE
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_0900_ai_ci;
+-- `mysql-chal`.Users definition
+CREATE TABLE `Users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_type_id` bigint NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `last_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `Users_UNIQUE` (`username`),
+  KEY `Users_UserTypes_FK` (`user_type_id`),
+  CONSTRAINT `Users_UserTypes_FK` FOREIGN KEY (`user_type_id`) REFERENCES `UserTypes` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 
 CREATE TABLE `mysql-chal`.Scores (
@@ -37,3 +36,15 @@ CREATE TABLE `mysql-chal`.Scores (
 ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_0900_ai_ci;
+
+INSERT INTO `mysql-chal`.UserTypes (name) VALUES
+	 ('Dev'),
+	 ('User');
+
+
+INSERT INTO `mysql-chal`.Users (user_type_id,username,last_updated) VALUES
+	 (2,'Wolven531','2025-05-21 04:16:49'),
+	 (1,'Roughnick','2025-05-21 04:16:49'),
+	 (1,'Jhectf','2025-05-21 04:16:49'),
+	 (2,'Zorven','2025-05-21 04:16:49'),
+	 (2,'Merlin1320','2025-05-21 04:16:49');
